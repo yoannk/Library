df8c2512485e2493a5bb86f483ae222bf5e49b2f
Yoann Kergall <yoann.kergall@gmail.com>
2020-03-19 00:00:37 +0100

    ajout Models

--- LibraryData/Models/Patron.cs
+++ LibraryData/Models/Patron.cs
@@ -13,6 +13,7 @@ namespace LibraryData.Models
         public DateTime DateOfBirth { get; set; }
         public string TelephoneNumber { get; set; }
 
-        //public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
     }
 }
