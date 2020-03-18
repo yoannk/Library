07b21418af298307651e4caa06f227f77cae2419
Yoann Kergall <yoann.kergall@gmail.com>
2020-03-18 22:25:28 +0100

    add Patron class

--- LibraryData/LibraryContext.cs
+++ LibraryData/LibraryContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace LibraryData
@@ -6,5 +7,7 @@ namespace LibraryData
     public class LibraryContext : DbContext
     {
         public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set; }
     }
 }
---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        //public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
