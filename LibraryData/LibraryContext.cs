930af6fe71951c4497a9ed70eb180b86eead28b6
Yoann Kergall <yoann.kergall@gmail.com>
2020-03-18 22:06:32 +0100

    add LibraryContext

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+    }
+}
