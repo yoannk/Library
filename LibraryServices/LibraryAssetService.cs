168e42f9cdfb7a790dba8dcfe173657dd479d7f8
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-13 22:40:50 +0200

    implémentation méthodes de l'interface ILibraryAsset

--- LibraryServices/LibraryAssetService.cs
+++ LibraryServices/LibraryAssetService.cs
@@ -1,12 +1,73 @@
 ﻿using LibraryData;
 using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LibraryServices
 {
-    public class LibraryAssetService
+    public class LibraryAssetService : ILibraryAsset
     {
-        
+        private LibraryContext _context;
+
+        public LibraryAssetService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(LibraryAsset newAsset)
+        {
+            _context.Add(newAsset);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<LibraryAsset> GetAll()
+        {
+            return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location);
+        }
+
+        public LibraryAsset GetById(int id)
+        {
+            return GetAll().FirstOrDefault(asset => asset.Id == id);
+        }
+
+        public LibraryBranch GetCurrentLocation(int id)
+        {
+            return GetById(id).Location;
+        }
+
+        public string GetDeweyIndex(int id)
+        {
+            return _context.Books.FirstOrDefault(book => book.Id == id).DeweyIndex ?? "";
+        }
+
+        public string GetIsbn(int id)
+        {
+            return _context.Books.FirstOrDefault(book => book.Id == id).ISBN ?? "";
+        }
+
+        public string GetTitle(int id)
+        {
+            return _context.LibraryAssets.FirstOrDefault(book => book.Id == id).Title;
+        }
+
+        public string GetType(int id)
+        {
+            var book = _context.LibraryAssets.OfType<Book>().Where(b => b.Id == id);
+
+            return book.Any() ? "Book" : "Video";
+        }
+
+        public string GetAuthorOrDirector(int id)
+        {
+            var isBook = _context.LibraryAssets.OfType<Book>().Where(asset => asset.Id == id).Any();
+
+            return isBook ?
+                _context.Books.FirstOrDefault(book => book.Id == id).Author :
+                _context.Videos.FirstOrDefault(book => book.Id == id).Director;
+        }
     }
 }
